6ce6592d57846245b2931262514e0ff061a5cae7
JohannesWorks <JohannesWorks@gmx.de>
2024-11-28 10:25:50 +0100

    Runde button ...

--- src/components/NavigationComp.vue
+++ src/components/NavigationComp.vue
@@ -1,48 +1,115 @@
 <template>
-    <div :class="navClass">
-      <router-link to="/equipment" class="nav-button">Ausrüstung</router-link>
-      <router-link to="/camera" class="nav-button">Kamera</router-link>
-      <router-link to="/tppa" class="nav-button">TPPA</router-link>
-      <router-link to="/autofocus" class="nav-button">Autofokus</router-link>
-      <router-link to="/sequencer" class="nav-button">Sequenzer</router-link>
-      <router-link to="/misc" class="nav-button">Sonstiges</router-link>
-    </div>
-  </template>
+  <div :class="navClass">
+    <router-link
+      to="/equipment"
+      class="nav-button"
+      :class="{ 'active-button': $route.path === '/equipment' }"
+      title="Ausrüstung"
+    >
+      <HomeIcon class="icon w-6 h-6" />
+    </router-link>
+    <router-link
+      to="/camera"
+      class="nav-button"
+      :class="{ 'active-button': $route.path === '/camera' }"
+      title="Kamera"
+    >
+      <CameraIcon class="icon w-6 h-6" />
+    </router-link>
+    <router-link
+      to="/tppa"
+      class="nav-button"
+      :class="{ 'active-button': $route.path === '/tppa' }"
+      title="TPPA"
+    >
+      <MapIcon class="icon w-6 h-6" />
+    </router-link>
+    <router-link
+      to="/autofocus"
+      class="nav-button"
+      :class="{ 'active-button': $route.path === '/autofocus' }"
+      title="Autofokus"
+    >
+      <CubeIcon class="icon w-6 h-6" />
+    </router-link>
+    <router-link
+      to="/sequencer"
+      class="nav-button"
+      :class="{ 'active-button': $route.path === '/sequencer' }"
+      title="Sequenzer"
+    >
+      <EyeIcon class="icon w-6 h-6" />
+    </router-link>
+    <router-link
+      to="/misc"
+      class="nav-button"
+      :class="{ 'active-button': $route.path === '/misc' }"
+      title="Sonstiges"
+    >
+      <CogIcon class="icon w-6 h-6" />
+    </router-link>
+  </div>
+</template>
+
   
   <script>
-  export default {
-    data() {
-      return {
-        isPortrait: window.matchMedia("(orientation: portrait)").matches,
-      };
+import {
+  HomeIcon,
+  CameraIcon,
+  MapIcon,
+  CubeIcon,
+  EyeIcon,
+  CogIcon,
+} from '@heroicons/vue/24/outline';
+
+export default {
+  components: {
+    HomeIcon,
+    CameraIcon,
+    MapIcon,
+    EyeIcon,
+    CubeIcon,
+    CogIcon,
+  },
+  data() {
+    return {
+      isPortrait: window.matchMedia("(orientation: portrait)").matches,
+    };
+  },
+  computed: {
+    navClass() {
+      return this.isPortrait
+        ? 'flex justify-around p-2 bg-gray-800'
+        : 'flex flex-col items-center p-2 bg-gray-800 fixed right-0 top-0 h-full';
     },
-    computed: {
-      navClass() {
-        return this.isPortrait
-          ? 'flex justify-around p-2 bg-gray-800' // Navigation oben
-          : 'flex flex-col items-center p-2 bg-gray-800 fixed right-0 top-0 h-full'; // Navigation rechts
-      },
+  },
+  mounted() {
+    window.addEventListener('resize', this.handleResize);
+  },
+  beforeUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      this.isPortrait = window.matchMedia("(orientation: portrait)").matches;
     },
-    mounted() {
-      window.addEventListener('resize', this.handleResize);
-    },
-    beforeUnmount() {
-      window.removeEventListener('resize', this.handleResize);
-    },
-    methods: {
-      handleResize() {
-        this.isPortrait = window.matchMedia("(orientation: portrait)").matches;
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .nav-button {
-    @apply px-4 py-2 bg-gray-700 text-white rounded hover:bg-gray-600;
-    display: block;
-    text-align: center;
-    margin: 4px;
-  }
-  </style>
+  },
+};
+</script>
+
+<style scoped>
+.nav-button {
+  @apply w-12 h-12 bg-gray-700 text-white rounded-full hover:bg-gray-600;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 4px;
+  transition: all 0.1s ease-in-out;
+}
+
+.active-button {
+  @apply rounded-md bg-gray-600; /* Eckig und eine dunklere Farbe */
+}
+</style>
+
   
