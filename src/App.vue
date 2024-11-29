59440a10d86448d2769dbfefc0147dc8ac302f36
JohannesWorks <JohannesWorks@gmx.de>
2024-11-29 19:26:41 +0100

    meta viewport für kein zoom

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,21 @@
 </template>
 
 <script>
+import { useHead } from '@vueuse/head';
 import NavigationComp from './components/NavigationComp.vue';
 
 export default {
+  setup() {
+    useHead ({
+      title: "NINA WebApp",
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+        },
+     ],
+    })
+  },
   components: {
     NavigationComp,
 
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'; // Importiert Tailwind CSS
-
-createApp(App).use(router).mount('#app')
+import { createHead } from '@unhead/vue';
+const head = createHead()
+createApp(App).use(head).use(router).mount('#app')
