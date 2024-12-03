c07fd255600ebc983207c8178248340fa289e55f
JohannesWorks <JohannesWorks@gmx.de>
2024-12-03 18:09:58 +0100

    TPPA Verbindung über proxy

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,14 @@ module.exports = defineConfig({
         logLevel: "debug", 
       },
     },
+    proxy: {
+      "/v2/tppa": {
+        target: "ws://localhost:1888", // URL des API-Servers
+        secure: false, // SSL-Zertifikate ignorieren (optional)
+        changeOrigin: true, // Erzwingt Cross-Origin-Header-Anpassungen
+        pathRewrite: { "^/v2/tppa": "/v2/tppa" }, // (Optional) Umschreiben des Pfads
+        logLevel: "debug", 
+      },
+    },
   },
 });
