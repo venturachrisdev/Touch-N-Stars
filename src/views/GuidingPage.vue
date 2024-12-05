04bd5690f9f48a9b84430d8d67cd19c566eea716
JohannesWorks <JohannesWorks@gmx.de>
2024-12-05 08:17:10 +0100

    Guiding kann gestartet und gestopt werden

--- src/views/GuidingPage.vue
+++ src/views/GuidingPage.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="container text-center">
-    <h5 class="text-xl  font-bold text-white mb-4">Guiding</h5>
+  <div class="container flex tems-center justify-center">
+    <div class="container max-w-md ">
+      <h5 class="text-xl text-center font-bold text-white mb-4">Guiding</h5>
+        <div> <!-- für die Abfrage ob Verbunden-->
+        </div>
+        <div><!-- Wenn verbunden dann hier der Inhalt-->
+          <div class="flex space-x-4">
+            <button
+              class="flex h-10 w-full rounded-md text-white font-medium transition-colors bg-cyan-700 items-center justify-center disabled:opacity-50"
+              @click="guiderStartStop('start')"
+            >
+              Start 
+            </button>
+            <button
+              class="flex h-10 w-full rounded-md text-white font-medium transition-colors bg-cyan-700 items-center justify-center disabled:opacity-50"
+              @click="guiderStartStop('stop')"
+            >
+              Stop 
+            </button>
+          </div>
 
-    
-    
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-
-//import apiService from "@/services/apiService";
+import apiService from "@/services/apiService";
 
 export default {
   components: {
@@ -17,14 +34,24 @@ export default {
   },
   data() {
     return {
-
+      StatusRun:false,
     };
   },
   async mounted() {
     
   },
   methods: {
-
+    async guiderStartStop(befehl) {
+        try{
+          await apiService.guiderAction(befehl);
+          console.log("Guider starten");
+        }
+        catch (error) {
+          console.error("Fehler :", error.response?.data || error);
+          this.mountStatus = "Fehler ";
+        }
+     },
   },
+  
 };
 </script>
