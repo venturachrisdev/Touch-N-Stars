9d274a6f222582ad5ece5c31b054a6e3c55b6e0d
JohannesWorks <JohannesWorks@gmx.de>
2024-12-10 15:01:20 +0100

    Code aufgeräumt

--- server/server.py
+++ server/server.py
@@ -1,20 +1,65 @@
 from flask import Flask, send_from_directory, request, Response, jsonify
 import requests
 import pandas as pd
-import json
 import os
 
 app = Flask(__name__, static_folder='static')  # Statische Dateien in 'static'
 
+# API Url für NINA
 BASE_API_URL = "http://localhost:1888/v2/api"
 
-# API-Route für NGC-Daten
+# NGC Daten für die API einlesen
 data = pd.read_csv('./katalog/NGC.csv',sep=';')
 data2 = pd.read_csv('./katalog/addendum.csv', sep=';')  
 data = pd.concat([data, data2], ignore_index=True)
 
+# ------------- API zur Objetksuche -----------------------
+@app.route('/api/ngc/search', methods=['GET'])
+def search_ngc():
+    query = request.args.get('query', '').strip().lower()
+    limit = request.args.get('limit', 100)
 
-# Proxy-Route
+    # Validierung der Eingaben
+    if not query:
+        return {"error": "Query parameter is required"}, 400
+    try:
+        limit = int(limit)
+        if limit <= 0:
+            return {"error": "Limit must be a positive integer"}, 400
+    except ValueError:
+        return {"error": "Invalid limit parameter"}, 400
+
+     # Bestimmen, welche Spalte anhand des Prefixes durchsucht werden soll
+    if query.startswith('m'):
+        search_column = 'M'
+        search_value = query[1:]  # entfernt das 'm' am Anfang
+    elif query.startswith('ngc'):
+        search_column = 'Name'
+        search_value = query[3:]  # entfernt das 'ngc' am Anfang
+    elif query.startswith('ic'):
+        search_column = 'Name'
+        search_value = query[2:]  # entfernt das 'ic' am Anfang
+    else:
+         search_column = 'Common names'
+         search_value = query[0:]
+
+    # Filtere die Daten basierend auf der Spalte und dem gekürzten Suchwert
+    results = data[
+        data[search_column].astype(str).str.lower().str.contains(search_value, na=False)
+    ].head(limit)
+
+    # Wähle nur die gewünschten Spalten
+    selected_columns = ['Name', 'Type', 'RA', 'Dec', 'M', 'Common names']
+    results_filtered = results[selected_columns]
+
+    # Ersetze NaN durch None
+    results_temp = results_filtered.fillna(value="")
+    results_cleaned = results_temp.replace("", None)
+
+    # Rückgabe als gültiges JSON
+    return jsonify(results_cleaned.to_dict(orient='records'))
+
+# ------------------ Proxy-Route zur NINA API ----------------------
 @app.route('/v2/api/<path:endpoint>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def proxy(endpoint):
     target_url = f"{BASE_API_URL}/{endpoint}"
@@ -41,56 +86,7 @@ def proxy(endpoint):
         return {"error": "Failed to connect to target API"}, 500
 
 
-
-@app.route('/api/ngc/search', methods=['GET'])
-def search_ngc():
-    query = request.args.get('query', '').strip().lower()
-    limit = request.args.get('limit', 100)
-
-    # Validierung der Eingaben
-    if not query:
-        return {"error": "Query parameter is required"}, 400
-    try:
-        limit = int(limit)
-        if limit <= 0:
-            return {"error": "Limit must be a positive integer"}, 400
-    except ValueError:
-        return {"error": "Invalid limit parameter"}, 400
-
-     # Bestimmen, welche Spalte anhand des Prefixes durchsucht werden soll
-    if query.startswith('m'):
-        search_column = 'M'
-        search_value = query[1:]  # entfernt das 'm' am Anfang
-    elif query.startswith('ngc'):
-        search_column = 'Name'
-        search_value = query[3:]  # entfernt das 'ngc' am Anfang
-    elif query.startswith('ic'):
-        search_column = 'IC'
-        search_value = query[2:]  # entfernt das 'ic' am Anfang
-    else:
-         search_column = 'Common names'
-         search_value = query[0:]
-       
-
-
-    # Filtere die Daten basierend auf der Spalte und dem gekürzten Suchwert
-    results = data[
-        data[search_column].astype(str).str.lower().str.contains(search_value, na=False)
-    ].head(limit)
-
-
-    # Wähle nur die gewünschten Spalten
-    selected_columns = ['Name', 'Type', 'RA', 'Dec', 'M', 'Common names']
-    results_filtered = results[selected_columns]
-
-    # Ersetze NaN durch None
-    results_temp = results_filtered.fillna(value="")
-    results_cleaned = results_temp.replace("", None)
-
-    # Rückgabe als gültiges JSON
-    return jsonify(results_cleaned.to_dict(orient='records'))
-
-# Route für die Vue-App
+# ---------------------Route für die Vue-App-----------------------
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve_vue(path):
