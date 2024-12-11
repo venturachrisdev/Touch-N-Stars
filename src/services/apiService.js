78b98d0e8190c95c4db0dead66c782f493fbf62d
JohannesWorks <JohannesWorks@gmx.de>
2024-12-11 19:45:05 +0100

    api erweitert zum schwenken

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -7,17 +7,17 @@ const NGCS_URL = "http://192.168.2.128:5000/api/ngc/search";
 
 
 const apiService = {
-    // Backend-Erreichbarkeitsprüfung
-    async isBackendReachable() {
-      try {
-        const response = await axios.get(`${BASE_URL}/version`);
-        return response.status === 200;
-      } catch (error) {
-        console.error("Fehler beim Erreichen des Backends:", error.message);
-        return false;
-      }
-    },
-  
+  // Backend-Erreichbarkeitsprüfung
+  async isBackendReachable() {
+    try {
+      const response = await axios.get(`${BASE_URL}/version`);
+      return response.status === 200;
+    } catch (error) {
+      console.error("Fehler beim Erreichen des Backends:", error.message);
+      return false;
+    }
+  },
+
   // Verbindungen ------------------------------
   // Kameraaktionen
   cameraAction(action) {
@@ -48,59 +48,63 @@ const apiService = {
 
   //Foto aufnehmen ------------------------------------
   async startCapture(duration) {
-      return axios
-        .get(`${BASE_URL}/equipment/camera/capture`, {
-          params: { duration, getResult: false },
-        })
-        .then((response) => response.data)
-        .catch((error) => {
-          console.error("Fehler beim Starten der Aufnahme:", error);
-          throw error;
-        });
-    },
-  
-    async getCaptureResult() {
-      return axios
-        .get(`${BASE_URL}/equipment/camera/capture`, {
-          params: { 
-            getResult: true,
-            quality: 80,
-           },
-        })
-        .then((response) => response.data)
-        .catch((error) => {
-          console.error("Fehler beim Abrufen des Bildes:", error);
-          throw error;
-        });
-    },
-    // Focuser --------------------------------------------------------
-    async moveFocuser(position) {
-      return axios
-        .get(`${BASE_URL}/equipment/focuser/move`, {
-          params: { position },
-        })
-        .then((response) => response.data)
-        .catch((error) => {
-          console.error("Fehler beim Steuern des OAZ:", error);
-          throw error;
-        });
-    },
+    return axios
+      .get(`${BASE_URL}/equipment/camera/capture`, {
+        params: { duration, getResult: false },
+      })
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Fehler beim Starten der Aufnahme:", error);
+        throw error;
+      });
+  },
 
-        // Slew and Center --------------------------------------------------------
-        async slewAndCenter(RAangle,DECangle) {
-          return axios
-            .get(`${BASE_URL}/framing/set-coordinates`, {
-              params: { 
-                RAangle,
-                DECangle,
-               },
-            })
-            .then((response) => response.data)
-            .catch((error) => {
-              console.error("Fehler beim Steuern der Montierung:", error);
-              throw error;
-            });
+  async getCaptureResult() {
+    return axios
+      .get(`${BASE_URL}/equipment/camera/capture`, {
+        params: {
+          getResult: true,
+          quality: 80,
         },
+      })
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Fehler beim Abrufen des Bildes:", error);
+        throw error;
+      });
+  },
+  // Focuser --------------------------------------------------------
+  async moveFocuser(position) {
+    return axios
+      .get(`${BASE_URL}/equipment/focuser/move`, {
+        params: { position },
+      })
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Fehler beim Steuern des OAZ:", error);
+        throw error;
+      });
+  },
+
+  // Slew and Center --------------------------------------------------------
+  async slewAndCenter(RAangle, DECangle) {
+    try {
+      // Koordinaten setzen
+      await axios.get(`${BASE_URL}/framing/set-coordinates`, {
+        params: {
+          RAangle,
+          DECangle,
+        },
+      });
+  
+      // Slew-Befehl senden
+      const response = await axios.get(`${BASE_URL}/framing/slew`);
+      return response.data;
+    } catch (error) {
+      console.error("Fehler beim Steuern der Montierung:", error);
+      throw error;
+    }
+  },
 
   // NGC-Suche:
   async searchNGC(query, limit = 10) {
