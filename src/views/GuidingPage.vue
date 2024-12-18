c3651495bd7763fea8546809dfc18dc335c8d4fe
JohannesWorks <JohannesWorks@gmx.de>
2024-12-18 22:02:05 +0100

    Guider angepasst

--- src/views/GuidingPage.vue
+++ src/views/GuidingPage.vue
@@ -2,7 +2,7 @@
   <div class="container flex items-center justify-center">
     <div class="container max-w-md">
       <h5 class="text-xl text-center font-bold text-white mb-4">Guiding</h5>
-      <div v-if="!isConnected" class="text-red-500">
+      <div v-if="!store.guiderInfo.Connected" class="text-red-500">
         <p>Bitte Guider verbinden</p>
       </div>
       <div v-else>
@@ -23,9 +23,9 @@
         </div>
         <div class="mt-5">
           <p class="">RMS Fehler in Arcseconds</p>
-          <p>RA: {{ RmsErrorRaArcseconds }}</p>
-          <p>DEC: {{ RmsErrorDecArcseconds }}</p>
-          <p>Total: {{ RmsErrorTotalArcseconds }}</p>
+          <p>RA: {{ store.guiderInfo.RMSError.Dec.Arcseconds.toFixed(2) }}</p>
+          <p>DEC: {{ store.guiderInfo.RMSError.RA.Arcseconds.toFixed(2) }}</p>
+          <p>Total: {{ store.guiderInfo.RMSError.Total.Arcseconds.toFixed(2) }}</p>
         </div>
         <div class="mt-5">
           <rmsGraph />
@@ -36,16 +36,12 @@
 </template>
 
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from 'vue'
 import apiService from "@/services/apiService";
 import rmsGraph from '../components/GuiderGraph.vue';
+import { apiStore } from '@/store/store';
 
-const isConnected = ref(false);
-const RmsErrorRaArcseconds = ref(null);
-const RmsErrorDecArcseconds = ref(null);
-const RmsErrorTotalArcseconds = ref(null);
+const store = apiStore();
 
-let intervalId = null;
 
 async function guiderStartStop(befehl) {
   try {
@@ -56,48 +52,7 @@ async function guiderStartStop(befehl) {
   }
 }
 
-async function fetchGuiderInfo(initialFetch = false) {
-  try {
-    const response = await apiService.guiderAction("info");
-    if (response.Success) {
-      const data = response.Response;
-      isConnected.value = data.Connected;
-      if (isConnected.value) {
-        RmsErrorRaArcseconds.value = parseFloat(data.RMSError.RA.Arcseconds.toFixed(2));
-        RmsErrorDecArcseconds.value = parseFloat(data.RMSError.Dec.Arcseconds.toFixed(2));
-        RmsErrorTotalArcseconds.value = parseFloat(data.RMSError.Total.Arcseconds.toFixed(2));
-      }
 
-      if (initialFetch && isConnected.value) {
-        console.log("Initial fetch done.");
-      }
-    } else {
-      console.error("Fehler in der API-Antwort:", response.Error);
-    }
-  } catch (error) {
-    console.error("Fehler beim Abrufen der Guider-Informationen:", error);
-  }
-}
-
-function startFetchingInfo() {
-  fetchGuiderInfo(true);
-  intervalId = setInterval(() => fetchGuiderInfo(false), 1000);
-}
-
-function stopFetchingInfo() {
-  if (intervalId) {
-    clearInterval(intervalId);
-    intervalId = null;
-  }
-}
-
-onMounted(() => {
-  startFetchingInfo();
-});
-
-onBeforeUnmount(() => {
-  stopFetchingInfo();
-});
 </script>
 
 <style scoped></style>
