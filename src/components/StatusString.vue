32dc362ab484919af51aede122e56afb1ac329f4
JohannesWorks <JohannesWorks@gmx.de>
2024-12-18 13:53:20 +0100

    vue3

--- src/components/StatusString.vue
+++ src/components/StatusString.vue
@@ -1,36 +1,27 @@
 <template>
+  <div v-if="isEnabled" class="flex items-center space-x-4 border border-gray-500 rounded-xl min-h-7 pl-2 pr-2">
+    <div class="flex items-center space-x-3 text-xs">
+      <strong class="text-gray-400">{{ Name }}</strong><p>{{ Value }}</p>
+    </div>
+  </div>
+</template>
 
-        <div v-if="isEnabled" class="flex items-center space-x-4 border border-gray-500 rounded-xl min-h-7 pl-2 pr-2 "> 
-            <div class="flex items-center space-x-3 text-xs ">
-                <bold class=" text-gray-400">{{Name}} </bold><p>{{Value}}</p>
-            </div>
-        </div>
-  
+<script setup>
+/* eslint-disable */
+const props = defineProps({
+  isEnabled: {
+    type: Boolean,
+    required: true,
+  },
+  Name: {
+    type: String,
+    default: 'Name:',
+  },
+  Value: {
+    type: String,
+    default: '1234',
+  },
+})
+</script>
 
-  </template>
-  
-  <script>
-
-  
-  export default {
-    props: {
-      isEnabled: {
-        type: Boolean,
-        required: true,
-      },
-      Name: {
-        type: String,
-        default: 'Name:',
-      },
-      Value: {
-        type: String,
-        default: '1234', 
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-
-  </style>
-  
+<style scoped></style>
