b7448a1f36f2028e8544d26d3b05e0af997edc6f
JohannesWorks <JohannesWorks@gmx.de>
2024-12-18 13:18:12 +0100

    vue3

--- src/components/ConnectionButton.vue
+++ src/components/ConnectionButton.vue
@@ -1,45 +1,43 @@
 <template>
-    <div >
-      <p class="text-white mb-2 ">{{ statusText }}</p>
-      <button
-        @click="toggleConnection"
-        class="min-w-64 min-h-10 rounded-md text-white font-medium transition-colors"
-        :class="isConnected ? 'bg-red-900' : 'bg-cyan-700'"
-      >
-        {{ isConnected ? disconnectText : connectText }}
-      </button>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      isConnected: {
-        type: Boolean,
-        required: true,
-      },
-      statusText: {
-        type: String,
-        required: true,
-      },
-      connectText: {
-        type: String,
-        default: "Verbinden",
-      },
-      disconnectText: {
-        type: String,
-        default: "Trennen",
-      },
-      onToggle: {
-        type: Function,
-        required: true,
-      },
-    },
-    methods: {
-      toggleConnection() {
-        this.onToggle();
-      },
-    },
-  };
-  </script>
-  
+  <div>
+    <p class="text-white mb-2">{{ statusText }}</p>
+    <button
+      @click="toggleConnection"
+      class="min-w-64 min-h-10 rounded-md text-white font-medium transition-colors"
+      :class="isConnected ? 'bg-red-900' : 'bg-cyan-700'"
+    >
+      {{ isConnected ? disconnectText : connectText }}
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  isConnected: {
+    type: Boolean,
+    required: true,
+  },
+  statusText: {
+    type: String,
+    required: true,
+  },
+  connectText: {
+    type: String,
+    default: "Verbinden",
+  },
+  disconnectText: {
+    type: String,
+    default: "Trennen",
+  },
+  onToggle: {
+    type: Function,
+    required: true,
+  },
+})
+
+function toggleConnection() {
+  props.onToggle()
+}
+</script>
