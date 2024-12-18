39fa791dbad5b6bd3554d73699cd0a4d3fb0ee33
JohannesWorks <JohannesWorks@gmx.de>
2024-12-18 13:35:19 +0100

    Vue3

--- src/components/infoFocuser.vue
+++ src/components/infoFocuser.vue
@@ -1,84 +1,76 @@
 <template>
-    <div v-if="!isConnected" class="text-red-500 ">
-        <p>Bitte Fokusierer verbinden</p>
-    </div>
-    <div v-else class=" gap-2 ">
-        <StatusString :isEnabled="currentPosition" Name="Aktuelle Position:" :Value="currentPosition" />
-        <StatusString :isEnabled="temperature" Name="Temperatur:" :Value="temperature" />
-        <StatusBool class="col-start-1" :isEnabled="!isMoving" enabledText="Beweg sich" disabledText="Steht" />
-        <StatusBool :isEnabled="isSettling" enabledText="Backlash Korrektur" disabledText="Backlash Korrektur" />
-    </div>
+  <div v-if="!isConnected" class="text-red-500">
+    <p>Bitte Fokusierer verbinden</p>
+  </div>
+  <div v-else class="gap-2">
+    <StatusString :isEnabled="currentPosition" Name="Aktuelle Position:" :Value="currentPosition" />
+    <StatusString :isEnabled="temperature" Name="Temperatur:" :Value="temperature" />
+    <StatusBool class="col-start-1" :isEnabled="!isMoving" enabledText="Bewegt sich" disabledText="Steht" />
+    <StatusBool :isEnabled="isSettling" enabledText="Backlash Korrektur" disabledText="Backlash Korrektur" />
+  </div>
 </template>
 
-<script>
-
+<script setup>
+/* eslint-disable */
+import { ref, watch, onMounted, onBeforeUnmount } from 'vue';
 import apiService from "@/services/apiService";
 import StatusBool from '../components/StatusBool.vue';
 import StatusString from '../components/StatusString.vue';
 
-export default {
-    components: {
-        StatusBool,
-        StatusString,
-    },
-    data() {
-        return {
-            currentPosition: null, // Aktuelle Position vom Server
-            temperature: null, // Aktuelle Temperatur
-            isMoving: false, // Status: Bewegt sich
-            isSettling: false, // Status: Wird eingestellt
-            isConnected: false, // Verbindungsstatus
-        };
-    },
-    props: {
-        modelValue: Boolean,
-    },
-    watch: {
-        isConnected(newVal) {
-            this.$emit("update:modelValue", newVal); // Bidirektionales Update
-        },
-    },
-    async mounted() {
-        // Hole die aktuelle Position beim Laden der Komponente
-        await this.fetchInfo(true);
-        // Starte das regelmäßige Abrufen der Informationen
-        this.startFetchingInfo();
-    },
-    beforeUnmount() {
-        // Stoppe den Intervall, wenn die Komponente zerstört wird
-        this.stopFetchingInfo();
-    },
-    methods: {
-        async fetchInfo() {
-            try {
-                const response = await apiService.focusAction("info"); // API-Aufruf
-                if (response.Success) {
-                    const data = response.Response;
-                    this.isConnected = data.Connected; // Verbindungsstatus setzen
-                    this.currentPosition = data.Position; // Setze aktuelle Position
-                    this.temperature = data.Temperature.toFixed(2); // Setze Temperatur
-                    this.isMoving = data.IsMoving; // Setze Bewegung-Status
-                    this.isSettling = data.IsSettling; // Setze Einstellungs-Status
-                    console.log(data);
-                    this.$emit("update:modelValue", this.isConnected);
-                } else {
-                    console.error("Fehler in der API-Antwort:", response.Error);
-                }
-            } catch (error) {
-                console.error("Fehler beim Abrufen der Mount-Informationen:", error);
-            }
-        },
-        startFetchingInfo() {
-            // Starte das Intervall für regelmäßige Abrufe
-            this.intervalId = setInterval(this.fetchInfo, 1000);
-        },
-        stopFetchingInfo() {
-            // Stoppe das Intervall
-            if (this.intervalId) {
-                clearInterval(this.intervalId);
-                this.intervalId = null;
-            }
-        },
-    },
-};
+const { modelValue } = defineProps({
+  modelValue: Boolean,
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const currentPosition = ref(null);
+const temperature = ref(null);
+const isMoving = ref(false);
+const isSettling = ref(false);
+const isConnected = ref(false);
+
+let intervalId = null;
+
+async function fetchInfo() {
+  try {
+    const response = await apiService.focusAction("info");
+    if (response.Success) {
+      const data = response.Response;
+      isConnected.value = data.Connected;
+      currentPosition.value = data.Position;
+      temperature.value = data.Temperature.toFixed(2);
+      isMoving.value = data.IsMoving;
+      isSettling.value = data.IsSettling;
+      emit("update:modelValue", isConnected.value);
+    } else {
+      console.error("Fehler in der API-Antwort:", response.Error);
+    }
+  } catch (error) {
+    console.error("Fehler beim Abrufen der Fokussierer-Informationen:", error);
+  }
+}
+
+function startFetchingInfo() {
+  intervalId = setInterval(fetchInfo, 1000);
+}
+
+function stopFetchingInfo() {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+}
+
+watch(isConnected, (newVal) => {
+  emit("update:modelValue", newVal);
+});
+
+onMounted(async () => {
+  await fetchInfo();
+  startFetchingInfo();
+});
+
+onBeforeUnmount(() => {
+  stopFetchingInfo();
+});
 </script>
