b3a40b22d1a8c86ce27b95be99d1c50c5a3f77d5
JohannesWorks <JohannesWorks@gmx.de>
2024-12-18 13:40:39 +0100

    vue3

--- src/components/infoMount.vue
+++ src/components/infoMount.vue
@@ -1,79 +1,75 @@
 <template>
-    <div v-if="!isConnected" class="text-red-500 ">
-        <p>Bitte Montierung verbinden</p>
+    <div v-if="!isConnected" class="text-red-500">
+      <p>Bitte Montierung verbinden</p>
     </div>
-    <div v-else class=" gap-2 ">
-        <StatusBool :isEnabled="!parkPosition" enabledText="Ausgeparkt" disabledText="Geparkt" />
-        <StatusBool :isEnabled="TrackingEnabled" enabledText="Tracking ist aktiv" disabledText="Tracking deaktiviert" />
-        <StatusBool :isEnabled="Slewing" enabledText="Montierung schwenkt" disabledText="Schwenkt nicht" />
+    <div v-else class="gap-2">
+      <StatusBool :isEnabled="!parkPosition" enabledText="Ausgeparkt" disabledText="Geparkt" />
+      <StatusBool :isEnabled="TrackingEnabled" enabledText="Tracking ist aktiv" disabledText="Tracking deaktiviert" />
+      <StatusBool :isEnabled="Slewing" enabledText="Montierung schwenkt" disabledText="Schwenkt nicht" />
     </div>
-</template>
-
-<script>
-
-import apiService from "@/services/apiService";
-import StatusBool from '../components/StatusBool.vue';
-
-export default {
-    components: {
-        StatusBool,
-    },
-    data() {
-        return {
-            parkPosition: true,
-            TrackingEnabled: false,
-            Slewing: false,
-            isConnected: false,
-        };
-    },
-    props: {
-        modelValue: Boolean,
-    },
-    watch: {
-        isConnected(newVal) {
-            this.$emit("update:modelValue", newVal); // Bidirektionales Update
-        },
-    },
-    async mounted() {
-        this.isConnected = true;
-        await this.fetchInfo();
-        this.startFetchingInfo();
-    },
-    beforeUnmount() {
-        // Stoppe den Intervall, wenn die Komponente zerstört wird
-        this.stopFetchingInfo();
-    },
-    methods: {
-        async fetchInfo() {
-            try {
-                const response = await apiService.mountAction("info"); 
-                if (response.Success) {
-                    const data = response.Response;
-                    this.isConnected = data.Connected;
-                    this.parkPosition = data.AtPark; 
-                    this.TrackingEnabled = data.TrackingEnabled;
-                    this.Slewing = data.Slewing;
-
-                } else {
-                    this.isConnected = false;
-                    console.error("Fehler in der API-Antwort:", response.Error);
-                }
-            } catch (error) {
-                this.isConnected = false;
-                console.error("Fehler beim Abrufen der Mount-Informationen:", error);
-            }
-        },
-        startFetchingInfo() {
-            // Starte das Intervall für regelmäßige Abrufe
-            this.intervalId = setInterval(this.fetchInfo, 1000);
-        },
-        stopFetchingInfo() {
-            // Stoppe das Intervall
-            if (this.intervalId) {
-                clearInterval(this.intervalId);
-                this.intervalId = null;
-            }
-        },
-    },
-};
-</script>
+  </template>
+  
+  <script setup>
+  /* eslint-disable */
+  import { ref, watch, onMounted, onBeforeUnmount } from 'vue';
+  import apiService from "@/services/apiService";
+  import StatusBool from '../components/StatusBool.vue';
+  
+  const { modelValue } = defineProps({
+    modelValue: Boolean,
+  });
+  
+  const emit = defineEmits(['update:modelValue']);
+  
+  const parkPosition = ref(true);
+  const TrackingEnabled = ref(false);
+  const Slewing = ref(false);
+  const isConnected = ref(false);
+  
+  let intervalId = null;
+  
+  async function fetchInfo() {
+    try {
+      const response = await apiService.mountAction("info");
+      if (response.Success) {
+        const data = response.Response;
+        isConnected.value = data.Connected;
+        parkPosition.value = data.AtPark;
+        TrackingEnabled.value = data.TrackingEnabled;
+        Slewing.value = data.Slewing;
+      } else {
+        isConnected.value = false;
+        console.error("Fehler in der API-Antwort:", response.Error);
+      }
+    } catch (error) {
+      isConnected.value = false;
+      console.error("Fehler beim Abrufen der Mount-Informationen:", error);
+    }
+  }
+  
+  function startFetchingInfo() {
+    intervalId = setInterval(fetchInfo, 1000);
+  }
+  
+  function stopFetchingInfo() {
+    if (intervalId) {
+      clearInterval(intervalId);
+      intervalId = null;
+    }
+  }
+  
+  watch(isConnected, (newVal) => {
+    emit("update:modelValue", newVal);
+  });
+  
+  onMounted(async () => {
+    isConnected.value = true;
+    await fetchInfo();
+    startFetchingInfo();
+  });
+  
+  onBeforeUnmount(() => {
+    stopFetchingInfo();
+  });
+  </script>
+  
