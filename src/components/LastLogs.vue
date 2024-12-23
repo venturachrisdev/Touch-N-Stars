b36f0f06d2844c3a11add3438770138fedcd865c
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2024-12-23 13:52:32 +0100

    Anpassungen

--- src/components/LastLogs.vue
+++ src/components/LastLogs.vue
@@ -9,41 +9,34 @@
     </div>
 
     <!-- Tabelle anzeigen, wenn Daten verfügbar sind -->
-    <table v-else class="min-w-full table-auto border-collapse border border-gray-800 text-gray-300 text-sm">
-      <thead>
-        <tr class="bg-gray-800">
-          <th class="border border-gray-300 px-4 py-2 text-left">Timestamp</th>
-          <th class="border border-gray-300 px-4 py-2 text-left">Level</th>
-          <th class="border border-gray-300 px-4 py-2 text-left">Message</th>
-          <th class="border border-gray-300 px-4 py-2 text-left hidden md:table-cell">Member</th>
-          <th class="border border-gray-300 px-4 py-2 text-left hidden md:table-cell">Source</th>
-          <th class="border border-gray-300 px-4 py-2 text-left hidden md:table-cell">Line</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="(entry, index) in log.logs"
-          :key="index"
-          class="odd:bg-gray-900 even:bg-gray-600"
-        >
-          <td class="border border-gray-300 px-4 py-2">{{ formatTimestamp(entry.timestamp) }}</td>
-          <td
-            class="border border-gray-300 px-4 py-2"
-            :class="{
+    <div v-else class="w-full overflow-x-auto">
+      <table class="w-full table-fixed border-collapse border border-gray-800 text-gray-300 text-sm">
+        <thead>
+          <tr class="bg-gray-800">
+            <th class="border border-gray-300 px-4 py-2 text-left w-1/10">Timestamp</th>
+            <th class="border border-gray-300 px-4 py-2 text-left w-1/10">Level</th>
+            <th class="border border-gray-300 px-4 py-2 text-left w-2/5">Message</th>
+            <th class="border border-gray-300 px-4 py-2 text-left hidden landscape:table-cell w-1/10">Member</th>
+            <th class="border border-gray-300 px-4 py-2 text-left hidden landscape:table-cell w-1/10">Line</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(entry, index) in log.logs" :key="index" class="odd:bg-gray-900 even:bg-gray-600">
+            <td class="border border-gray-300 px-4 py-2">{{ formatTimestamp(entry.timestamp) }}</td>
+            <td class="border border-gray-300 px-4 py-2" :class="{
               'text-green-600': entry.level === 'INFO',
               'text-red-600': entry.level === 'ERROR',
               'text-yellow-600': entry.level === 'WARNING',
-            }"
-          >
-            {{ entry.level }}
-          </td>
-          <td class="border border-gray-300 px-4 py-2">{{ entry.message }}</td>
-          <td class="border border-gray-300 px-4 py-2 hidden md:table-cell">{{ entry.member }}</td>
-          <td class="border border-gray-300 px-4 py-2 hidden md:table-cell">{{ entry.source }}</td>
-          <td class="border border-gray-300 px-4 py-2 hidden md:table-cell">{{ entry.line }}</td>
-        </tr>
-      </tbody>
-    </table>
+            }">
+              {{ entry.level }}
+            </td>
+            <td class="border border-gray-300 px-4 py-2">{{ entry.message }}</td>
+            <td class="border border-gray-300 px-4 py-2 hidden landscape:table-cell">{{ entry.member }}</td>
+            <td class="border border-gray-300 px-4 py-2 hidden landscape:table-cell">{{ entry.line }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -68,7 +61,7 @@ function formatTimestamp(timestamp) {
 async function fetchLastLogs() {
   try {
     const response = await apiService.getLastLogs("10");
-    
+
     // Überprüfen Sie die Struktur der API-Antwort
     // Angenommen, die Logs befinden sich direkt in response.logs
     if (response && Array.isArray(response.logs)) {
--- src/components/testTest.vue
+++ src/components/testTest.vue
@@ -1,14 +1,14 @@
 <template>
-    <div>
+    <div class="container ">
         <h1>Test</h1>
-        <LastMessage />
-        <p>123</p>
+      
+       
         <LastLogs />
     </div>
 </template>
 
 <script setup>
-import LastMessage from './LastMessage.vue';
+
 import LastLogs from './LastLogs.vue';
 
 </script>
