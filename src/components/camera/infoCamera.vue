00c71d70749066627971962ee67ceaa6c8cf67dd
acocalypso <st.luzifer6663@hotmail.com>
2025-01-03 00:35:37 +0100

    Camera Translated

--- src/components/camera/infoCamera.vue
+++ src/components/camera/infoCamera.vue
@@ -1,19 +1,19 @@
 <template>
 
   <div v-if="!store.cameraInfo.Connected" class="text-red-500">
-    <p>Bitte Kamera verbinden</p>
+    <p>{{ $t('components.camera.connect') }}</p>
   </div>
   <div v-else-if="showAllInfo" class="gap-2">
-    <StatusString :isEnabled="store.cameraInfo.Name" Name="Name:" :Value="store.cameraInfo.Name" />
-    <StatusString :isEnabled="true" Name="Gain:" :Value="store.cameraInfo.Gain" />
-    <StatusString :isEnabled="store.cameraInfo.Offset" Name="Offset:" :Value="store.cameraInfo.Offset" />
-    <StatusString :isEnabled="isTemperatureEnabled" Name="Temperatur:" :Value="formattedTemperature" />
-    <StatusBool class="col-start-1" :isEnabled="store.cameraInfo.IsExposing" enabledText="Aufnahme läuft"
-      disabledText="Kamera standby" />
+    <StatusString :isEnabled="store.cameraInfo.Name" :Name="$t('components.camera.name')" :Value="store.cameraInfo.Name" />
+    <StatusString :isEnabled="true" :Name="$t('components.camera.gain')" :Value="store.cameraInfo.Gain" />
+    <StatusString :isEnabled="store.cameraInfo.Offset" :Name="$t('components.camera.offset')" :Value="store.cameraInfo.Offset" />
+    <StatusString :isEnabled="isTemperatureEnabled" :Name="$t('components.camera.temperature')" :Value="formattedTemperature" />
+    <StatusBool class="col-start-1" :isEnabled="store.cameraInfo.IsExposing" :enabledText="$t('components.camera.capture_running')"
+      :disabledText="$t('components.camera.standby')" />
     <StatusBool v-if="store.cameraInfo.CanSetTemperature" :isEnabled="store.cameraInfo.CoolerOn"
-      enabledText="Kühler aktiv" disabledText="Kühler aus" />
+      :enabledText="$t('components.camera.cooler_on')" :disabledText="$t('components.camera.cooler_off')" />
     <StatusBool v-if="store.cameraInfo.HasDewHeater" :isEnabled="store.cameraInfo.DewHeaterOn"
-      enabledText="Tauheizung aktiv" disabledText="Tauheizung aus" />
+      :enabledText="$t('components.camera.dew_heater_on')" :disabledText="$t('components.camera.dew_heater_off')" />
   </div>
   <div v-else-if="showOnlyExposing" class="gap-2">
     <StatusBool :isEnabled="store.cameraInfo.IsExposing" enabledText="Aufnahme läuft" disabledText="Kamera standby" />
--- src/components/camera/settingsCamera.vue
+++ src/components/camera/settingsCamera.vue
@@ -2,13 +2,13 @@
 
     <div class="flex items-center gap-2">
         <div class="flex flex-col  border border-gray-500  p-1 pb-2 rounded-lg">
-            <label for="exposure" class="text-xs mb-1 text-gray-400">Belichtungszeit:</label>
+            <label for="exposure" class="text-xs mb-1 text-gray-400">{{ $t('components.camera.exposure_time') }}</label>
             <input id="exposure" v-model.number="cameraStore.exposureTime" type="number"
                 class="w-28 text-black px-3 h-8  border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-700"
                 placeholder="sek" />
         </div>
         <div class="flex flex-col  border border-gray-500  p-1 pb-2 rounded-lg ">
-            <label for="gain" class="text-xs mb-1 text-gray-400">Gain / Iso:</label>
+            <label for="gain" class="text-xs mb-1 text-gray-400">{{ $t('components.camera.gain_iso') }}</label>
             <div v-if="store.cameraInfo.Gains && store.cameraInfo.Gains.length > 0">
                 <select id="gain" v-model.number="cameraStore.gain"
                     class="w-28 text-black px-3 h-8  border border-gray-300 rounded-lg  focus:outline-none focus:ring-2 focus:ring-cyan-700">
