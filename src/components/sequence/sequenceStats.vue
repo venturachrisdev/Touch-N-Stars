c50d45d59f0b7a3fb00cdd85cafae696dab6169c
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2025-01-05 14:37:24 +0100

    Stats Sequence fix

--- src/components/sequence/sequenceStats.vue
+++ src/components/sequence/sequenceStats.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="flex justify-center">
-<div class="max-w-md landscape:max-w-2xl ">
- <LastSequenceImg />
- <SequenzGraph class="pt-4" />
-</div>
-</div>
+    <div v-if="store.imageHistoryInfo && store.imageHistoryInfo.length > 0" class="max-w-md landscape:max-w-2xl ">
+      <LastSequenceImg />
+      <SequenzGraph class="pt-4" />
+    </div>
+    <div v-else class="p-4 bg-red-500/10 border border-red-500/30 rounded-lg">
+      <p class="text-red-400 font-medium">{{ $t('components.sequence.noSequenceData') }}</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import LastSequenceImg from './LastSequenceImg.vue';
 import SequenzGraph from './SequenzGraph.vue';
+import { apiStore } from "@/store/store";
 
+const store = apiStore();
 
 </script>
 
