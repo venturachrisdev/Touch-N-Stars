7410a58fce6eaa0813cf3e5b87885885fe101adf
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2025-01-09 19:56:16 +0100

    Camera set offset

--- src/components/camera/settingsCamera.vue
+++ src/components/camera/settingsCamera.vue
@@ -23,6 +23,23 @@
                     placeholder="1" />
             </div>
         </div>
+        <div v-if="store.cameraInfo.CanSetOffset" class="flex flex-col  border border-gray-500  p-1 pb-2 rounded-lg ">
+            <label for="offset" class="text-xs mb-1 text-gray-400">{{ $t('components.camera.offset') }}</label>
+            <div v-if="store.cameraInfo.Offset && store.cameraInfo.Offset.length > 0">
+                <select id="offset" v-model.number="cameraStore.offset"
+                    class="w-28 text-black px-3 h-8  border border-gray-300 rounded-lg  focus:outline-none focus:ring-2 focus:ring-cyan-700">
+                    <option v-for="(value, key) in store.cameraInfo.Offset" :key="key" :value="key" @change="setOffset">
+                        {{ value }}
+                    </option>
+                </select>
+            </div>
+            <div v-else>
+                <input id="offset" v-model.number="cameraStore.offset" type="number"
+                    class="w-28 text-black px-3 h-8 border border-gray-300 rounded-lg  focus:outline-none focus:ring-2 focus:ring-cyan-700"
+                    placeholder="0" 
+                    @blur="setOffset"/>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -30,10 +47,21 @@
 //import { ref } from 'vue';
 import { apiStore } from '@/store/store';
 import { useCameraStore } from '@/store/cameraStore';
+import apiService from '@/services/apiService';
 
 const store = apiStore();
 const cameraStore = useCameraStore();
 
+async function setOffset() {
+    console.log(cameraStore.offset)
+    try{
+        console.log("aldfj");
+       const data = await apiService.profileChangeValue("CameraSettings-Offset",cameraStore.offset);
+       console.log(data);
+    }catch(error){
+        console.log("Fehler beim setzten des Offset")
+    }
+}
 
 
 
