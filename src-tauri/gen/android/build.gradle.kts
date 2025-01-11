b792c35af44d20603b7f553f950a5cca83364779
acocalypso <st.luzifer6663@hotmail.com>
2025-01-12 00:14:38 +0100

    update gradle to include singing key

--- src-tauri/gen/android/build.gradle.kts
+++ src-tauri/gen/android/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.io.FileInputStream
+
 buildscript {
     repositories {
         google()
@@ -6,6 +8,27 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.5.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
+    }
+}
+
+signingConfigs {
+    create("release") {
+        val keystorePropertiesFile = rootProject.file("key.properties")
+        val keystoreProperties = Properties()
+        if (keystorePropertiesFile.exists()) {
+            keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+        }
+
+        keyAlias = keystoreProperties["keyAlias"] as String
+        keyPassword = keystoreProperties["password"] as String
+        storeFile = file(keystoreProperties["storeFile"] as String)
+        storePassword = keystoreProperties["password"] as String
+    }
+}
+
+buildTypes {
+    getByName("release") {
+        signingConfig = signingConfigs.getByName("release")
     }
 }
 
