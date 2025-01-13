e5776428be10b444aef54b9ffb982a2d56cfd5f1
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2025-01-13 21:41:22 +0100

    show only the navbutton of the devices that are connected

--- src/components/NavigationComp.vue
+++ src/components/NavigationComp.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="top-0 h-16 bg-gray-800 shadow-md">
-    <div class="flex inset-x-0 mx-auto max-w-md h-16 items-center justify-around">
+    <div class="flex mx-auto max-w-md h-16 items-center justify-center">
       <router-link
         to="/equipment"
         class="nav-button"
@@ -9,51 +9,55 @@
       >
         <LinkIcon class="icon" />
       </router-link>
-      <router-link
-        to="/camera"
-        class="nav-button"
-        active-class="active-nav-button"
-        :title="$t('components.navigation.camera')"
-      >
-        <CameraIcon class="icon" />
-      </router-link>
-      <router-link
-        to="/autofocus"
-        class="nav-button"
-        active-class="active-nav-button"
-        :title="$t('components.navigation.autofocus')"
-      >
-        <EyeIcon class="icon" />
-      </router-link>
-
-      <router-link
-        to="/mount"
-        class="nav-button"
-        active-class="active-nav-button"
-        :title="$t('components.navigation.mount')"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="icon"
+      <div v-if="store.cameraInfo.Connected">
+        <router-link
+          to="/camera"
+          class="nav-button"
+          active-class="active-nav-button"
+          :title="$t('components.navigation.camera')"
         >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M6 21l6 -5l6 5" />
-          <path d="M12 13v8" />
-          <path
-            d="M3.294 13.678l.166 .281c.52 .88 1.624 1.265 2.605 .91l14.242 -5.165a1.023 1.023 0 0 0 .565 -1.456l-2.62 -4.705a1.087 1.087 0 0 0 -1.447 -.42l-.056 .032l-12.694 7.618c-1.02 .613 -1.357 1.897 -.76 2.905z"
-          />
-          <path d="M14 5l3 5.5" />
-        </svg>
-      </router-link>
-
+          <CameraIcon class="icon" />
+        </router-link>
+      </div>
+      <div v-if="store.focuserInfo.Connected">
+        <router-link
+          to="/autofocus"
+          class="nav-button"
+          active-class="active-nav-button"
+          :title="$t('components.navigation.autofocus')"
+        >
+          <EyeIcon class="icon" />
+        </router-link>
+      </div>
+      <div v-if="store.mountInfo.Connected">
+        <router-link
+          to="/mount"
+          class="nav-button"
+          active-class="active-nav-button"
+          :title="$t('components.navigation.mount')"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="icon"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M6 21l6 -5l6 5" />
+            <path d="M12 13v8" />
+            <path
+              d="M3.294 13.678l.166 .281c.52 .88 1.624 1.265 2.605 .91l14.242 -5.165a1.023 1.023 0 0 0 .565 -1.456l-2.62 -4.705a1.087 1.087 0 0 0 -1.447 -.42l-.056 .032l-12.694 7.618c-1.02 .613 -1.357 1.897 -.76 2.905z"
+            />
+            <path d="M14 5l3 5.5" />
+          </svg>
+        </router-link>
+      </div>
       <div v-if="store.domeInfo.Connected">
         <router-link
           to="/dome"
@@ -108,44 +112,45 @@
           </svg>
         </router-link>
       </div>
-
-      <router-link
-        to="/guider"
-        class="nav-button"
-        active-class="active-nav-button"
-        :title="$t('components.navigation.guider')"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="icon icon-tabler icons-tabler-outline icon-tabler-viewfinder"
+      <div v-if="store.guiderInfo.Connected">
+        <router-link
+          to="/guider"
+          class="nav-button"
+          active-class="active-nav-button"
+          :title="$t('components.navigation.guider')"
         >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-          <path d="M12 3l0 4" />
-          <path d="M12 21l0 -3" />
-          <path d="M3 12l4 0" />
-          <path d="M21 12l-3 0" />
-          <path d="M12 12l0 .01" />
-        </svg>
-      </router-link>
-
-      <router-link
-        to="/sequence"
-        class="nav-button"
-        active-class="active-nav-button"
-        :title="$t('components.navigation.sequence')"
-      >
-        <ListBulletIcon class="icon" />
-      </router-link>
-
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="icon icon-tabler icons-tabler-outline icon-tabler-viewfinder"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+            <path d="M12 3l0 4" />
+            <path d="M12 21l0 -3" />
+            <path d="M3 12l4 0" />
+            <path d="M21 12l-3 0" />
+            <path d="M12 12l0 .01" />
+          </svg>
+        </router-link>
+      </div>
+      <div v-if="store.sequenceIsLoaded">
+        <router-link
+          to="/sequence"
+          class="nav-button"
+          active-class="active-nav-button"
+          :title="$t('components.navigation.sequence')"
+        >
+          <ListBulletIcon class="icon" />
+        </router-link>
+      </div>
       <router-link
         to="/settings"
         class="nav-button"
