3bb66b3270fa00ba03fd50e2330706e2bc3cbcba
acocalypso <st.luzifer6663@hotmail.com>
2025-01-14 10:12:48 +0100

    Package update (#48)
    * remove server py
    
    * update packages & lint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,30 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    es2022: true
   },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     parser: '@babel/eslint-parser',
+    requireConfigFile: false
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'prettier/prettier': 'error',
+    'prettier/prettier': ['error', {
+      endOfLine: 'auto'
+    }]
   },
   globals: {
     defineProps: 'readonly',
     defineEmits: 'readonly',
     defineExpose: 'readonly',
-    withDefaults: 'readonly',
-  },
+    withDefaults: 'readonly'
+  }
 };
