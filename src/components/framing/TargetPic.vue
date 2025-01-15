9b576b574670d2b6959c546c5e0f0ed02b381f20
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2025-01-15 13:05:53 +0100

    Framingstore integrated in TargetPic

--- src/components/framing/TargetPic.vue
+++ src/components/framing/TargetPic.vue
@@ -14,50 +14,19 @@
 <script setup>
 import { ref, watch, onMounted } from 'vue';
 import apiService from '@/services/apiService';
+import { useFramingStore } from '@/store/framingStore';
 
-// eslint-disable-next-line
-const props = defineProps({
-  RAangleString: String,
-  DECangleString: String,
-});
-
+const framingStore = useFramingStore();
 const targetPic = ref(null);
-
-function hmsToDegrees(hmsString) {
-  const parts = hmsString.split(':');
-  const hours = parseInt(parts[0], 10);
-  const minutes = parseInt(parts[1], 10);
-  const seconds = parseInt(parts[2], 10);
-  return hours * 15 + minutes * (15 / 60) + seconds * (15 / 3600);
-}
-
-function dmsToDegrees(dmsString) {
-  const sign = dmsString.startsWith('-') ? -1 : 1;
-  const stripped = dmsString.replace('-', '');
-  const parts = stripped.split(':');
-
-  if (parts.length !== 3) {
-    throw new Error('Ungültiges Format. Erwartet: ±DD:MM:SS.s');
-  }
-
-  const degrees = parseFloat(parts[0]);
-  const minutes = parseFloat(parts[1]);
-  const seconds = parseFloat(parts[2]);
-
-  return sign * (degrees + minutes / 60 + seconds / 3600);
-}
 
 async function getTargetPic() {
   try {
-    const ra = hmsToDegrees(props.RAangleString);
-    const dec = dmsToDegrees(props.DECangleString);
-    console.log('RA: ', ra, 'DEC: ', dec);
-
-    console.log('Bild wird abgerufen');
+    const ra = framingStore.RAangleStringDeg;
+    const dec = framingStore.DECangleStringDeg;
+    console.log('Bild wird abgerufen', 'RA: ', ra, 'DEC: ', dec);
     if (targetPic.value) {
       URL.revokeObjectURL(targetPic.value);
     }
-
     targetPic.value = await apiService.searchTargetPic(200, 200, 2, ra, dec);
   } catch (error) {
     console.error('Fehler beim Abrufen des Bildes:', error);
@@ -69,8 +38,8 @@ function loadImage() {
 }
 
 // Beobachte Änderungen an RAangleString und DECangleString
-watch(() => props.RAangleString, loadImage);
-watch(() => props.DECangleString, loadImage);
+watch(() => framingStore.RAangleString, loadImage);
+watch(() => framingStore.DECangleString, loadImage);
 
 // Lade das Bild beim Mounten der Komponente
 onMounted(() => {
