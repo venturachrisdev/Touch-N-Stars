b25cbd3f61b0434ddf6666b07e835d6bb74ab45b
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2025-01-22 20:58:36 +0100

    fix use nina catche

--- src/components/testTest.vue
+++ src/components/testTest.vue
@@ -1,11 +1,60 @@
 <template>
-  <div>
-    <InfoFlatdevice />
-    <controlFlatdevice />
+  <div class="root">
+      <div class="container">
+          <div
+              class="target"
+              ref="targetRef"
+              :style="`width: ${width};height: ${height};`"
+          >
+              Target
+          </div>
+          <Moveable
+              ref="moveableRef"
+              :target="targetRef"
+              :draggable="draggable"
+              :throttleDrag="throttleDrag"
+              :edgeDraggable="edgeDraggable"
+              :startDragRotate="startDragRotate"
+              :throttleDragRotate="throttleDragRotate"
+              :rotatable="rotatable"
+              :throttleRotate="throttleRotate"
+              :rotationPosition="rotationPosition"
+              @drag="onDrag"
+              @rotate="onRotate"
+          />
+      </div>
   </div>
 </template>
 
 <script setup>
-import InfoFlatdevice from './flatdevice/InfoFlatdevice.vue';
-import controlFlatdevice from './flatdevice/controlFlatdevice.vue';
+import Moveable from "vue3-moveable";
+import { ref } from "vue";
+
+const width = "300px";
+const height = "200px";
+const draggable = true;
+const throttleDrag = 1;
+const edgeDraggable = false;
+const startDragRotate = 0;
+const throttleDragRotate = 0;
+const rotatable = true;
+const throttleRotate = 0;
+const rotationPosition = "top";
+const targetRef = ref(null);
+const moveableRef = ref(null);
+
+const position = ref({ left: 0, top: 0 });
+const onDrag = (e) => {
+  console.log(e.beforeTranslate)
+    e.target.style.transform = e.transform;
+};
+
+const onRotate = (e) => {
+  console.log(e.drag)
+    e.target.style.transform = e.drag.transform;
+};
+
+
 </script>
+
+
