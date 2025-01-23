1e35abee93da68ef06fb2038a4214b54be4b00b2
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2025-01-23 19:08:52 +0100

    switch set value

--- src/components/switch/ControlSwitch.vue
+++ src/components/switch/ControlSwitch.vue
@@ -1,8 +1,32 @@
 <template>
   <div v-for="WritableSwitche in store.switchInfo.WritableSwitches">
     <div v-if="WritableSwitche.Maximum === 1.0">
-      {{ WritableSwitche.Name }}
-      <toggleButton @click="setBool(WritableSwitche.Id,WritableSwitche.Value)" :status-value="WritableSwitche.TargetValue === 1" />
+      <div
+        class="flex flex-row items-center justify-between w-full border border-gray-500 p-2 rounded-lg"
+      >
+        {{ WritableSwitche.Name }}
+        <toggleButton
+          @click="setBool(WritableSwitche.Id, WritableSwitche.Value)"
+          :status-value="WritableSwitche.TargetValue === 1"
+        />
+      </div>
+    </div>
+  </div>
+  <div v-for="WritableSwitche in store.switchInfo.WritableSwitches">
+    <div v-if="WritableSwitche.Maximum > 1.0">
+      <div
+        class="flex flex-row items-center justify-between w-full border border-gray-500 p-2 rounded-lg"
+      >
+        {{ WritableSwitche.Name }}
+        <SetValue
+          @blur="
+            setValue(WritableSwitche.Id, $event, WritableSwitche.Minimum, WritableSwitche.Maximum)
+          "
+          :store-value="WritableSwitche.Value"
+          :min="WritableSwitche.Minimum"
+          :max="WritableSwitche.Maximum"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -10,6 +34,7 @@
 import { apiStore } from '@/store/store';
 import apiService from '@/services/apiService';
 import toggleButton from '@/components/helpers/toggleButton.vue';
+import SetValue from './SetValue.vue';
 
 const store = apiStore();
 
@@ -18,11 +43,26 @@ async function setBool(id, value) {
     //console.log('id: ', id, 'value: ', value)
     if (value === 1.0) {
       await apiService.setSwitch(id, 0);
-      console.log('Switch on: ', id);
+      console.log('Switch on ID: ', id);
     } else {
       await apiService.setSwitch(id, 1);
-      console.log('Switch off: ', id);
+      console.log('Switch off ID: ', id);
     }
+  } catch (error) {
+    console.log('Feher beim setzen des Switches ');
+  }
+}
+
+async function setValue(id, value, valueMin, valueMax) {
+  try {
+    if (value < valueMin) {
+      value = valueMin;
+    }
+    if (value > valueMax) {
+      value = valueMax;
+    }
+    await apiService.setSwitch(id, value);
+    console.log('Value set: ', value);
   } catch (error) {
     console.log('Feher beim setzen des Switches ');
   }
