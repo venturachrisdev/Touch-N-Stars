b7673b43e2211d2a3c7191f67896f3f6f6079fe2
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2025-02-11 20:49:04 +0100

    adjustments

--- src/components/flatassistant/AutoExposure.vue
+++ src/components/flatassistant/AutoExposure.vue
@@ -26,7 +26,9 @@
     v-show="flatsStore.status.CompletedIterations > -1"
     class="flex flex-col w-full max-w-md space-y-2 mt-4 border border-gray-700 rounded-lg bg-gradient-to-br from-gray-800 to-gray-900 shadow-lg p-5"
   >
+  <div class="flex justify-center items-center p-2 border border-gray-500 rounded-lg bg-gray-800">
     <getStatus />
+  </div>
     <LastImage />
   </div>
 </template>
