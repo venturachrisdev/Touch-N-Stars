c508e09d4408db387769a8d364b0e06e5ed89e07
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2025-02-13 19:13:59 +0100

    utils.js

--- src/components/camera/CenterHere.vue
+++ src/components/camera/CenterHere.vue
@@ -49,6 +49,7 @@ import { useFramingStore } from '@/store/framingStore';
 import { useSettingsStore } from '@/store/settingsStore';
 import { useCameraStore } from '@/store/cameraStore';
 import apiService from '@/services/apiService';
+import { degreesToHMS, degreesToDMS, rad2deg } from '@/utils/utils.js';
 
 const framingStore = useFramingStore();
 const cameraStore = useCameraStore();
@@ -170,40 +171,6 @@ function calculateRaDec() {
   console.log(currentRA , currentDec);
 }
 
-function degreesToHMS(deg) {
-  const totalHours = deg / 15;
-  const h = Math.floor(totalHours);
-  const remainingHours = totalHours - h;
-  const totalMinutes = remainingHours * 60;
-  const m = Math.floor(totalMinutes);
-  const remainingMinutes = totalMinutes - m;
-  const s = remainingMinutes * 60;
-
-  const hStr = String(h);
-  const mStr = String(m).padStart(2, '0');
-  const sStr = s.toFixed(1).padStart(4, '0');
-
-  return `${hStr}:${mStr}:${sStr}`;
-}
-function degreesToDMS(deg) {
-  const sign = deg < 0 ? '-' : '+';
-  deg = Math.abs(deg);
-  const d = Math.floor(deg);
-  const remainingDeg = deg - d;
-  const totalMinutes = remainingDeg * 60;
-  const m = Math.floor(totalMinutes);
-  const remainingMinutes = totalMinutes - m;
-  const s = remainingMinutes * 60;
-
-  const dStr = String(d).padStart(2, '0');
-  const mStr = String(m).padStart(2, '0');
-  const sStr = s.toFixed(1).padStart(4, '0');
-
-  return `${sign}${dStr}:${mStr}:${sStr}`;
-}
-function rad2deg(rad) {
-  return rad * (180 / Math.PI);
-}
 </script>
 
 <style scoped>
