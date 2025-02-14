4faaba79b82b4f33c8885a255a18ae53d51e58e7
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2025-02-14 11:25:16 +0100

    setting for use platesolve

---
+++ src/components/camera/setSolve.vue
@@ -0,0 +1,23 @@
+<template>
+  <toggleButton
+    @click="toggleSolve"
+    :status-value="settingsStore.camera.useSolve"
+    class="pr-5 pl-5 justify-center"
+  />
+</template>
+<script setup>
+import { useSettingsStore } from '@/store/settingsStore';
+import toggleButton from '@/components/helpers/toggleButton.vue';
+
+const settingsStore = useSettingsStore();
+
+function toggleSolve() {
+  if (settingsStore.camera.useSolve) {
+    ssettingsStore.camera.useSolve = false;
+    console.log('Platesolve off');
+  } else {
+    settingsStore.camera.useSolve = true;
+    console.log('Platesolve on');
+  }
+}
+</script>
